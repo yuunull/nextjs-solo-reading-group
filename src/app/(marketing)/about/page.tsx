1f44b2549c2e24c7417d67dca6a32c702636fdf5
yuunull <yuunull2525@gmail.com>
2024-06-24 22:33:11 +0900

    Project Organization and File Colocation

--- src/app/(marketing)/about/page.tsx
+++ src/app/(marketing)/about/page.tsx
@@ -1,7 +1,13 @@
+import Button from "@/app/_components/button";
 import React from "react";
 
 const page = () => {
-  return <div>about page</div>;
+  return (
+    <>
+      <div>about page</div>
+      <Button />
+    </>
+  );
 };
 
 export default page;
