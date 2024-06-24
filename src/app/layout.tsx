1c9721b20378c97616eb5608538a4c1c29051cb1
yuunull <yuunull2525@gmail.com>
2024-06-25 07:15:05 +0900

    feat:Font Optimization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,11 @@
+import { Roboto } from "next/font/google";
 import "./global.css";
+
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -6,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ja">
+    <html lang="ja" className={roboto.className}>
       <body>
         {/* Layout UI */}
         <main>{children}</main>
