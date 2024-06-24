c5faa02ae389c58232146050a54267f1a769986c
yuunull <yuunull2525@gmail.com>
2024-06-25 06:23:13 +0900

    feat:Server Actions and Mutations

--- src/app/actions.tsx
+++ src/app/actions.tsx
@@ -1,15 +1,8 @@
 "use server";
 
-import { redirect } from "next/navigation";
-import { revalidatePath } from "next/cache";
-
-export async function createPost(id: string) {
-  try {
-    // Call database
-  } catch (error) {
-    // Handle errors
-  }
-
-  revalidatePath("/posts"); // Update cached posts
-  redirect(`/post/${id}`); // Navigate to the new post page
+export async function createUser(prevState: any, formData: FormData) {
+  // ...
+  return {
+    message: "Please enter a valid email",
+  };
 }
